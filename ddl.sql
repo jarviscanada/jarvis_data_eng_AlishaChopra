cbc8e7831074f70109841ad3591c6aef62ecd1d8
Alisha Chopra <alisha.s.chopra@gmail.com>
2021-02-09 19:38:57 +0000

    creation of host_info and resource_usage tables

---
+++ ddl.sql
@@ -0,0 +1,27 @@
+
+
+CREATE TABLE IF NOT EXISTS PUBLIC.host_info 
+(
+	id SERIAL PRIMARY KEY,
+	hostname varchar(300) NOT NULL,
+	cpu_number INT NOT NULL,
+	cpu_architecture varchar(100) NOT NULL,
+	cpu_model varchar(350) NOT NULL,
+	cpu_mhz numeric(10,3) NOT NULL,
+	L2_cache INT NOT NULL,
+	Total_memory bigint NOT NULL,
+	curr_timestamp timestamp NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS PUBLIC.resource_usage
+(
+	usage_timestamp timestamp NOT NULL,
+	host_id int NOT NULL,
+	memory_free bigint NOT NULL,
+	cpu_idle numeric(3,2)  NOT NULL,
+	cpu_kernel numeric(3,2)  NOT NULL,
+	disk_io int NOT NULL,
+	disk_available bigint NOT NULL,
+	FOREIGN KEY(host_id) REFERENCES host_info(id)
+);
+
