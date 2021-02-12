e252e040764fa84244a13302daac9712fa4fdc52
Alisha Chopra <alisha.s.chopra@gmail.com>
2021-02-12 18:50:32 +0000

    first commit for sql data maipulation

---
+++ queries.sql
@@ -0,0 +1,49 @@
+SELECT 
+cpu_number,
+id as host_id,
+total_memory, 
+rank() over(Partition by cpu_number order by Total_memory desc) 
+FROM host_info;
+
+
+
+create function calculate_percentage(totalmemory numeric,free_memory numeric, OUT percentage numeric) As $$
+BEGIN
+
+
+	percentage := ((totalmemory - free_memory)/total_memory)*100;
+ 
+END;
+$$ LANGUAGE plpgsql;
+
+	
+SELECT 
+info.id, 
+hostname, 
+to_timestamp((extract('epoch' from usage.usage_timestamp)::int/300)*300) as "timestamp", 
+Round(AVG(calculate_percentage(cast(total_memory as numeric),cast(memory_free as numeric)))::numeric,2) as "avg_used_mem_percentage" 
+FROM 
+host_info info INNER JOIN resource_usage usage 
+ON info.id=usage.host_id 
+GROUP BY 
+info.id,
+to_timestamp((extract('epoch' from usage.usage_timestamp)::int/300)*300),
+hostname 
+ORDER BY timestamp;
+
+
+
+SELECT host_id, "ts", count(host_id) as "num_data_points"
+FROM 
+(SELECT host_id,hostname, 
+to_timestamp((extract('epoch' from usage.usage_timestamp)::int/300)*300) as "ts"
+FROM
+resource_usage usage INNER JOIN host_info info
+ON usage.host_id=info.id) as t 
+GROUP BY 
+host_id,
+"ts",
+hostname
+having count(host_id)<3
+order by "ts";
+
